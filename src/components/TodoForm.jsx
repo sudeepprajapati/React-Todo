a7a0699ab450bb6db13ce604e93ce84e54833139
Sudeep <sudeeppprajapati@gmail.com>
2024-02-04 15:13:29 +0530

    first commit

---
+++ src/components/TodoForm.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react'
+import { useTodo } from '../contexts/TodoContext';
+
+
+export default function TodoForm() {
+  const [todo, setTodo] = useState("")
+  const { addTodo } = useTodo()
+  const add = (e) => {
+    e.preventDefault()
+    if (!todo) return
+    addTodo({ todo, completed: false })
+    setTodo("")
+  }
+  return (
+    <form onSubmit={add} className="flex ">
+      <input
+        type="text"
+        placeholder="Write Todo..."
+        className="w-full border bg-rose-600 border-black/10 rounded-l-lg px-3 outline-none duration-150 py-1.5"
+        value={todo}
+        onChange={(e) => setTodo(e.target.value)}
+      />
+      <button type="submit" className="rounded-r-lg px-3 py-1 bg-gray-400 text-black shrink-0">
+        Add
+      </button>
+    </form>
+  );
+}
+
+// export default TodoForm;
